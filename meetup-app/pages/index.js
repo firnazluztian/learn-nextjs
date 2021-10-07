b67e34bf67ead7701527a8b2c5d9da15e37f77bd
firnazluztian <firnazluztian@gmail.com>
2021-10-07 17:08:01 +0700

    render list of dummy meetups

--- meetup-app/pages/index.js
+++ meetup-app/pages/index.js
@@ -1,5 +1,36 @@
+import MeetUpList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image: 'https://pix10.agoda.net/hotelImages/4869553/0/35b103e869655a2959fac36f614fa08e.jpg?s=1024x768',
+    address: 'kuta beach, Bali',
+    description: 'this is bali in here we have so many beaches'
+  },
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image: 'https://pix10.agoda.net/hotelImages/4869553/0/35b103e869655a2959fac36f614fa08e.jpg?s=1024x768',
+    address: 'kuta beach, Bali',
+    description: 'this is bali in here we have so many beaches'
+  },
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image: 'https://pix10.agoda.net/hotelImages/4869553/0/35b103e869655a2959fac36f614fa08e.jpg?s=1024x768',
+    address: 'kuta beach, Bali',
+    description: 'this is bali in here we have so many beaches'
+  }
+]
+
 function HomePage() {
-  return <h1>hello im home page</h1>
+  return <>
+    <div style={{margin: '0 auto', width: 700}}>
+      <MeetUpList meetups={DUMMY_MEETUPS} />
+    </div>
+    
+  </>
 }
 
 export default HomePage;
